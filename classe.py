8ffc09f31e011c8301ade23dfe0a1b93d08e25d2
EricPossato <ericalp@al.insper.edu.br>
2021-05-26 17:23:51 -0300

    Update classe.py
    Criação da classe player

--- classe.py
+++ classe.py
@@ -1,12 +1,15 @@
 import pygame
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y, assets):
+    def __init__(self, assets):
         super().__init__()
         self.image = assets['player']
         self.rect = self.image.get_rect()
-        self.rect.x = x*30
-        self.rect.y = y*30
+        self.speedx = 0
+        self.speedy = 0
+    def update (self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
 
 class Parede(pygame.sprite.Sprite):
     def __init__(self, x, y, assets):
