e0daccd869c91cda2d21b95c267afa21bf2a16b3
Gabriel <gabrielrn@al.insper.edu.br>
2021-05-26 16:42:30 -0300

    muita coisa

--- classe.py
+++ classe.py
@@ -4,7 +4,7 @@ import pygame
 class Parede(pygame.sprite.Sprite):
     def __init__(self, x, y, assets):
         super().__init__()
-        self.image = assets['parede_img']
+        self.image = assets['parede']
         self.rect = self.image.get_rect()
-        self.rect.x = x*20
-        self.rect.y = y*20
+        self.rect.x = x*30
+        self.rect.y = y*30
